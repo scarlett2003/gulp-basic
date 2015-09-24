d6817f7992f1da84f6621f2256f65ccf3f806a6f
vialley-imac <jovilee09@gmail.com>
2015-09-25 02:45:28 +0800

    finish basic gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifycess = require('gulp-minify-css'),
-	uglify = require('gulp-uglify'),
-	// compass = require('gulp-compass'),
-	rename = require('gulp-rename'),
-	notify = require('gulp-notify');
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ * https://987.tw/2014/07/09/gulpru-men-zhi-nan/ 中譯版
+ */
 
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
 
 //  流程 input src -> pipe function -> output dest
@@ -15,39 +26,73 @@ var gulp = require('gulp'),
 //  !css/style.css -> excludes style.css file
 //  *.+(js|css) -> Matches all files in the root directory ending in .js or .css
 
-// 19:34
-
-// Script Task
-gulp.task('scripts', function() {
-	// console.log('It worked Dude!');
-	gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
-		.pipe(rename({suffix:'.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest('app/js'));
-});
-
-
-
 // gulp-ruby-sass
 gulp.task('styles', function() {
   return sass('app/scss/style.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('app/css'))
     .pipe(rename({ suffix: '.min' }))
-    // .pipe(minifycss())
-    // .pipe(gulp.dest('app/css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('app/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 
-// Watch Task
-gulp.task('watch', function(){
-	gulp.watch('app/js/**/*.js', ['scripts']);
-	gulp.watch('app/scss/**/*.scss', ['compass']);
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('app/js/**/*.js')
+    // .pipe(jshint('.jshintrc'))
+    // .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('app/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 
+// Images
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('app/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
 
+
+// Clean 執行這個，以下資料夾會被清空
+// gulp.task('clean', function(cb) {
+//     del(['app/css', 'app/js', 'app/images'], cb)
+// });
+
+// Default task
+// gulp.task('default', ['clean'], function() {
+//     gulp.start('styles', 'scripts', 'images');
+// });
 
 // Default Task
-gulp.task('default',['scripts', 'watch', 'compass']);
+gulp.task('default',['scripts', 'styles', 'images']);
+
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('app/scss/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('app/js/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('app/images/**/*', ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['app/**']).on('change', livereload.changed);
+
+});
+
+
